8f4fa0626acdbd168534724d5b91feb6c5e2ce80
SikyChen <chensiqi247@gmail.com>
2022-06-23 23:32:33 +0800

    add: codeGenerator

--- src/codeGenerator.js
+++ src/codeGenerator.js
@@ -1,6 +1,92 @@
+/**
+ * 代码生成器
+ * Personal
+ * @param { Object } ast 转换后的新 AST 语法树
+ * @returns { String } 生成的新代码
+ */
+function codeGenerator(ast) {
 
-function codeGenerator() {
+  let code = '';
+
+  function process(node) {
+
+    if (node.type === 'ExpressionStatement') {
+      process(node.expression);
+      code += ';';
+    }
+
+    if (node.type === 'CallExpression') {
+      code += process(node.callee) + '(';
+      node.arguments.forEach((child, index) => {
+        process(child);
+        if (index < node.arguments.length - 1) {
+          code += ', ';
+        }
+      });
+      code += ')';
+    }
+
+    if (node.type === 'Identifier') {
+      code += node.name;
+    }
+
+    if (node.type === 'NumberLiteral') {
+      code += node.value;
+    }
+
+    if (node.type === 'StringLiteral') {
+      code += `"node.value"`;
+    }
+
+    throw new TypeError(node.type);
+  }
+
+  ast.body.forEach(process);
+
+  return code;
+}
+
+/**
+ * 代码生成器
+ * Original
+ * @param { Object } node 转换后的新 AST 语法树（根节点）
+ * @returns { String } 生成的新代码
+ */
+function codeGenerator(node) {
+
+  switch (node.type) {
+    
+    case 'Program':
+      return node.body.map(codeGenerator)
+        .join('\n');
+
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression) +
+        ';'
+      );
   
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator)
+          .join(', ') +
+        ')'
+      );
+  
+    case 'Identifier':
+      return node.name;
+  
+    case 'NumberLiteral':
+      return node.value;
+  
+    case 'StringLiteral':
+      return node.value;
+  
+    default:
+      throw new TypeError(node.type);
+  }
 }
 
 module.exports = codeGenerator;
